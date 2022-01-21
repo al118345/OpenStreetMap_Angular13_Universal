7a38d5401c00b697c58e4ac7cfd47282bdaa955e
ruben <rubenpeib@gmail.com>
2022-01-21 17:30:28 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
+
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+import { icon, Marker } from 'leaflet';
+
+export const DEFAULT_LAT = 48.20807;
+export const DEFAULT_LON =  16.37320;
+export const TITULO = 'Proyecto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularMap';
+export class AppComponent  implements OnInit {
+  private map:any;
+  @Input() lat: number = DEFAULT_LAT;
+  @Input() lon: number = DEFAULT_LON;
+  @Input() titulo: string = TITULO ;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+
+
+  private initMap(): void {
+    //configuración del mapa
+    this.map = L.map('map', {
+      center: [this.lat, this.lon],
+      attributionControl: false,
+      zoom: 14
+    });
+
+    //titulo
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://1938.com.es">Web Inteligencia Artificial</a>'
+    });
+
+    //marca con pop up
+    const lon = this.lon + 0.009;
+    const lat = this.lat + 0.009;
+    const marker = L.marker([lat + 0.005, lon + 0.005]).bindPopup(this.titulo);
+    marker.addTo(this.map);
+
+    //marca forma de circulo
+    const mark = L.circleMarker([this.lat, this.lon]).addTo(this.map);
+    mark.addTo(this.map);
+
+
+    //ruta
+    L.Routing.control({
+      router: L.Routing.osrmv1({
+        serviceUrl: `https://router.project-osrm.org/route/v1/`
+      }),
+      showAlternatives: true,
+      fitSelectedRoutes: false,
+      show: false,
+      routeWhileDragging: true,
+      waypoints: [
+        L.latLng(this.lat, this.lon),
+        L.latLng(lat, lon)
+      ]
+    }).addTo(this.map);
+    tiles.addTo(this.map);
+  }
 }
